699ea1c419e855588124982836a7773a958b74c8
Hazem Abouassi <hazem.abouassi@dci.education>
2023-01-31 09:54:57 +0100

    add BROWSER SHOW MODAL PROMISE

--- src/index.js
+++ src/index.js
@@ -1,15 +1,46 @@
 import "./css/style.css"; //importing css
-const modalBox = document.querySelector(".modal");
 
-const modal = () => {
+const modal = document.getElementById("myModal");
+
+const showModal = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("block");
+  }, 10000);
+});
+
+const getModal = async () => {
+  let result = await showModal;
+  modal.style.display = result;
+};
+
+getModal();
+
+//Bonus 1, close modal:
+modal.addEventListener("click", () => {
+  modal.style.display = "none";
+});
+
+//Bonus 2, animation end:
+
+// selecting the button
+const continueButton = document.getElementById("continue");
+
+// creating a function that returns the promise:
+const buttonPromise = () => {
   return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve("block");
-    }, 3000);
+    continueButton.addEventListener("animationend", () => {
+      // the className is to add the style from bootstrap
+      resolve({ className: "btn-info", alertMsg: "Continue to subscription!" });
+    });
   });
 };
 
-const section1 = document.querySelector("#myModal");
-section1.classlist.add(".modal__content");
+// the event handler that resolves the previous promise:
+const mouseOverHandler = async () => {
+  const result = await buttonPromise(); // coming from resolve {className: "btn-info", alertMsg: "Continue to subscription!"}
+  continueButton.classList.add(result.className);
+  alert(result.alertMsg);
+};
 
-modal().then((result) => (modalBox.style.display = result));
+// the event listener:
+continueButton.addEventListener("mouseover", mouseOverHandler);
